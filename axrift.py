607e2f692f98fbb616c522d2d0b224d9dec8b7ac
carbonsoft64 <94682959+carbonsoft64@users.noreply.github.com>
2022-05-09 21:44:01 +0300

    Axrift V31
    Added Ukrainian language support;
    Added list of officials fonts /fonts;
    Added /remove [font];
    Added info about contacts /support;
    Added \send[id] [msg] for sending messages to users by admins;
    Admin receive also sended commands;
    Fixed Markown bug.

--- axrift.py
+++ axrift.py
@@ -1,7 +1,8 @@
 import telebot
 from telebot import types
 import time
-bot = telebot.TeleBot('[TOKEN]')
+import os
+bot = telebot.TeleBot('5043884219:AAFWuoaG-ysmOCi0jcm75nADCIZeVD7V1S0')
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
@@ -41,6 +42,8 @@ def get_text_messages(message):
 	rootfile.close()
 	
 	#message reader & sender
+	result = ''
+	msggg = msg
 	if msg == '/start':
 		fctmp = open(idstr+'.fc', 'w')
 		fctmp.write(fca)
@@ -50,10 +53,12 @@ def get_text_messages(message):
 		defunifile.close()
 		
 		keyboard = types.InlineKeyboardMarkup()
-		key_en = types.InlineKeyboardButton(text='EN', callback_data='en')
-		keyboard.add(key_en)
-		key_ru = types.InlineKeyboardButton(text='RU', callback_data='ru')
-		keyboard.add(key_ru)
+		key_eng = types.InlineKeyboardButton(text='EN', callback_data='eng')
+		keyboard.add(key_eng)
+		key_rus = types.InlineKeyboardButton(text='RU', callback_data='rus')
+		keyboard.add(key_rus)
+		key_ukr = types.InlineKeyboardButton(text='UA', callback_data='ukr')
+		keyboard.add(key_ukr)
 		bot.send_message(message.from_user.id, text="Choose your language:", reply_markup=keyboard)
 	
 	elif msg.find('/font ') != -1:
@@ -74,11 +79,14 @@ def get_text_messages(message):
 			verdict = 'Successfully changed font to '
 			if lang == 'RUS':
 				verdict = 'Успешно изменен шрифт на '
+			if lang == 'UKR':
+				verdict = 'Успішно змінений шрифт на '
 			verdict += '`'+msg+'`'+'.'
 			bot.send_message(id, verdict, parse_mode='Markdown')
 		except Exception:
 			bot.send_message(id, 'Font not fond.')
 			
+	#admin
 	elif msg.find('\\replacehref ') != -1:
 		if root.find(idstr+' ') != -1:
 			hreffile = open('href.dat', 'w')
@@ -88,6 +96,21 @@ def get_text_messages(message):
 			bot.send_message(id, 'Href changed successfully!')
 		else:
 			bot.send_message(id, 'You haven\'t permissions to change href!')
+	elif msg.find('\\send')==0:
+		msg = msg[5:]
+		if root.find(idstr+' ') != -1:
+			try:
+				i = 0
+				while msg[i]!=' ': i+=1
+				sid = msg[:i]
+				msg = msg[i+1:]
+				bot.send_message(int(sid), msg)
+				bot.send_message(id, 'Successfully sended message to id\n'+sid+' with text\n'+msg)
+			except Exception:
+				bot.send_message(id, 'ERROR: False format or permission denied!')
+		else:
+			bot.send_message(id, 'You haven\'t permissions to send messages!')
+	#noadmin
 			
 	elif msg.find('/create ') != -1:
 		fontname = msg[8:]
@@ -97,12 +120,14 @@ def get_text_messages(message):
 			fontfile.close()
 			verdict = 'Font with this name alredy is used.'
 			if lang == 'RUS': verdict = 'Шрифт с таким именем уже существует.'
+			if lang == 'UKR': verdict = 'Шрифт з таким ім\'ям вже існує.'
 			bot.send_message(id, verdict)
 		except Exception:
 			try:
 				isint = int(fontname)
 				verdict = 'Font name can not be a number.'
 				if lang == 'RUS': verdict = 'Имя шрифта не должно складатся только из цифр.'
+				if lang == 'UKR': verdict = 'Назва шрифта не повинна складатися тільки із цифр.'
 				bot.send_message(id, verdict)
 			except Exception:
 				newfontfc = open(fontname+'.fc', 'a')
@@ -114,9 +139,10 @@ def get_text_messages(message):
 				fontrootfile.close()
 				verdict = 'Font successfully created. Add to it nice letters by using /add !'
 				if lang == 'RUS': verdict = 'Шрифт успешно создан. Добавте в него красивые буквы с помощью /add !'
+				if lang == 'UKR': verdict = 'Шрифт успішно створений. Добавте в нього красиві літери за допомогою /add !'
 				bot.send_message(id, verdict)
 				
-	elif(msg.find("/add ")==0):
+	elif(msg.find("/add")==0):
 		try:
 			msg = msg[5:]
 			#Removing fontname
@@ -131,13 +157,17 @@ def get_text_messages(message):
 				i+=1
 			nfontdfn = msg[:i]
 			nfontfc = msg[i+1:]
-			bot.send_message(id, 'Font Name: '+nfontname+ '\nDfn: '+nfontdfn+'\nFc: '+nfontfc)
+			#bot.send_message(id, 'Font Name: '+nfontname+ '\nDfn: '+nfontdfn+'\nFc: '+nfontfc)
 			try:
 				rootfontfile = open(nfontname+'.root', 'r')
 				rootfont = rootfontfile.read()
 				rootfontfile.close()
 				if id != int(rootfont):
-					bot.send_message(id, 'You haven\'t permissions to do this')
+					verdict = 'You haven\'t permissions to do this!'
+					if lang == 'RUS':
+						verdict = 'У вас недостаточно прав для этого!'
+					if lang == 'UKR': verdict = 'У вас недостатньо прав для цього!'
+					bot.send_message(id, verdict)
 				else:
 					fontdfnf = open(nfontname+'.dfn', 'r')
 					fontfcf = open(nfontname+'.fc', 'r')
@@ -152,16 +182,72 @@ def get_text_messages(message):
 					fontfcf.write(nfontfc+fontfc)
 					fontdfnf.close()
 					fontfcf.close()
-					bot.send_message(id, 'Successful adding changes')
+					verdict = 'Successful adding changes!'
+					if lang=='RUS':
+						verdict = 'Изменения успешно добавлены!'
+					if lang == 'UKR': verdict = 'Зміни успішно додані!'
+					bot.send_message(id, verdict)
 			except Exception:
-				bot.send_message(id, 'Font doesn\'t exists')
+				verdict = 'Font doesn\'t exists!'
+				if lang == 'RUS':
+					verdict = 'Этого шрифта не существует!'
+				if lang == 'UKR': verdict = 'Цього шрифта не існує!'
+				bot.send_message(id, verdict)
 		except Exception:
-			bot.send_message(id, 'Error: false format')
-	elif(msg == '/guide'):
-		verdict = 'Type and send me a text/message so I can make the letters look pretty!\n***Ext.  features:***\n• Change the font with /font \[font]!\n• Create your font with /create \[name]!\n• Add characters to your font with /add \[name_of_your_font] \[characters_to_replace  ] \[corresponding_characters_to_replace]\n***Standard fonts:***\n• Axrift\n• Rune\n• Copt'
+			verdict = 'Error: false format! Read /guide!'
+			if lang == 'RUS':
+				verdict = 'Ошибка: неправильный формат! Прочитайте /guide!'
+			if lang == 'UKR': verdict = 'Помилка: неправильний формат! Прочитайте /guide!'
+			bot.send_message(id, verdict)
+	elif msg.find('/remove')==0:
+		try:
+			msg = msg[8:]
+			rootfile = open(msg+'.root', 'r')
+			fontroot = int(rootfile.read())
+			rootfile.close()
+			if fontroot != id:
+				verdict ='You haven\'t permissions to do this!'
+				if lang == 'RUS': verdict = 'У вас недостаточно прав для этого!'
+				if lang == 'UKR': verdict = 'У вас недостатньо прав для цього!'
+				bot.send_message(id, verdict)
+			else:
+				os.remove(msg+'.root')
+				os.remove(msg+'.fc')
+				os.remove(msg+'.dfn')
+				verdict = 'Successfully removed font '
+				if lang == 'RUS': verdict = 'Успешно удален шрифт '
+				if lang == 'UKR': verdict = 'Успішно видалений шрифт '
+				bot.send_message(id, verdict+'\"'+msg+'\"')
+		except Exception:
+			verdict = 'Error: false format or font doesn\'t exists! Read /guide!'
+			if lang == 'RUS': verdict = 'Ошибка: неправильный формат или шрифт не существует! Прочитайте /guide!'
+			if lang == 'UKR': verdict = 'Помилка: неправильний формат або шрифт не існує! Прочитайте /guide!'
+			bot.send_message(id, verdict)
+	elif msg == '/fonts':
+		verdict = 'Official fonts:\n'
+		if lang == 'RUS': verdict = 'Оффициальные шрифты:\n'
+		if lang == 'UKR': verdict = 'Офіційні шрифти:\n'
+		
+		fontsfile = open('fonts.dat', 'a')
+		fontsfile.close()
+		
+		fontsfile = open('fonts.dat', 'r')
+		verdict += fontsfile.read()
+		fontsfile.close()
+		bot.send_message(id, verdict)
+	elif msg == '/support':
+		verdict = 'If you have some errors or ideas - contact with me: @likespro_official'
+		if lang=='RUS': verdict = 'Если у вас есть какие-то ошибки или идеи, свяжитесь со мной: @likespro_official'
+		if lang == 'UKR': verdict = 'Якщо у вас є якісь помилки або ідеї, то зв\'яжіться зі мною: @likespro_official'
+		bot.send_message(id, verdict)
+	elif msg == '/guide' :
+		verdict = 'Type and send me a text/message so I can make the letters look pretty!\n***Ext.  features:***\n• Change the font with /font \[font]!\n• Create your font with /create \[name]!\n• Add characters to your font with /add \[name_of_your_font] \[characters_to_replace  ] \[corresponding_characters_to_replace]\n• Remove your font with /remove \[name_of_your_font]\n***Standard fonts:***\n• Axrift\n• Rune\n• Copt'
 		if lang == 'RUS':
-			verdict = 'Наберите и отправте мне текст/сообщение, чтобы я сделал буквы красивыми!\n***Доп. возможности:***\n• Поменяйте шрифт командой /font \[шрифт]!\n• Создайте свой шрифт командой /create \[название]!\n• Добавте в свой шрифт символы командой /add \[название_вашего_шрифта] \[символы_которые_надо_заменить] \[соответствующие_символы_для_замены]\n***Стандартные шрифты:***\n• Axrift\n• Rune\n• Copt'
+			verdict = 'Наберите и отправте мне текст/сообщение, чтобы я сделал буквы красивыми!\n***Доп. возможности:***\n• Поменяйте шрифт командой /font \[шрифт]!\n• Создайте свой шрифт командой /create \[название]!\n• Добавте в свой шрифт символы командой /add \[название_вашего_шрифта] \[символы_которые_надо_заменить] \[соответствующие_символы_для_замены]\n• Удалите свой шрифт командой /remove \[название_вашего_шрифта]\n***Стандартные шрифты:***\n• Axrift\n• Rune\n• Copt'
+		if lang == 'UKR':
+			verdict = 'Наберіть та надішліть мені текст/повідомлення, щоб я зробив літери гарними!\n***Доп.  можливості:***\n• Поміняйте шрифт командою /font \[шрифт]!\n• Створіть свій шрифт командою /create \[назва]!\n• Додайте у свій шрифт символи командою /add \[назва_вашого_шрифту] \[символи_які_треба_замінити] [відповідні_символи_для_заміни]\n• Видаліть свій шрифт командою /remove \[назва_вашого_шрифту]\n***Офіційні шрифти:***\n• Axrift\n• Rune\n• Copt'
 		bot.send_message(id, verdict, parse_mode='Markdown')
+	
 	else:
 		ci = 0
 		result = ''
@@ -173,17 +259,21 @@ def get_text_messages(message):
 				result += msg[ci]
 			ci+=1
 		withHref = '['+result+']'+'('+href+')'
-		try:
-			bot.send_message(id, withHref, parse_mode='Markdown')
-		except Exception:
+		if result.find('[')!=-1 or result.find(']')!=-1:
 			bot.send_message(id, result)
-		bot.send_message(912002557, result+' - '+str(message.from_user.first_name)+'&'+str(message.from_user.last_name)+'@'+str(message.from_user.username)+'$'+str(message.from_user.id))
+		else:
+			try:
+				bot.send_message(id, withHref, parse_mode='Markdown')
+			except Exception:
+				bot.send_message(id, result)
+	
+	bot.send_message(912002557, msggg+'©'+result+' - '+str(message.from_user.first_name)+'&'+str(message.from_user.last_name)+'@'+str(message.from_user.username)+'$'+str(message.from_user.id))
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
 	idcall = call.message.chat.id
 	idcallstr = str(idcall)
-	if call.data == 'en':
+	if call.data == 'eng':
 		langfile = open(idcallstr+'.data', 'w')
 		langfile.write('ENG')
 		langfile.close()
@@ -191,7 +281,7 @@ def callback_worker(call):
 		axrift.write(idcallstr+' ')
 		axrift.close()
 		bot.send_message(idcall, 'Type and send to me a text/message to make nicer it! Learn more about axrift features by /guide')
-	if call.data == 'ru':
+	if call.data == 'rus':
 		langfile = open(idcallstr+'.data', 'w')
 		langfile.write('RUS')
 		langfile.close()
@@ -199,6 +289,14 @@ def callback_worker(call):
 		axrift.write(idcallstr+' ')
 		axrift.close()
 		bot.send_message(idcall, 'Наберите и отправте мне текст/сообщение, чтобы я сделал буквы красивыми! Узнайте больше про бота с помощью /guide')
+	if call.data == 'ukr':
+		langfile = open(idcallstr+'.data', 'w')
+		langfile.write('UKR')
+		langfile.close()
+		axrift=open('axrift.data', 'a')
+		axrift.write(idcallstr+' ')
+		axrift.close()
+		bot.send_message(idcall, 'Наберіть і відправте мені повідомлення/текст, щоб я зробив букви красивими! Узнайте більше про бота за допомогою /guide')
 
 #while True:
 #	try:
